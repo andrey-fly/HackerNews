9e03e4f5c2c8f7cc2a910340d2aca3e9e4fde6e1
Mukhin Andrey <mukhin.av.1997@gmail.com>
2021-11-26 19:10:25 +0300

    Add Web View of articles with using of WebKit

---
+++ HackerNews/Views/DetailView.swift
@@ -0,0 +1,25 @@
+//
+//  DetailView.swift
+//  HackerNews
+//
+//  Created by Andrey Mukhin on 26.11.2021.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+
