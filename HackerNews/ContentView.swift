e77b9d3f1f3cadf280230000ea88d6c713cc988b
Mukhin Andrey <mukhin.av.1997@gmail.com>
2021-11-26 18:09:24 +0300

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Andrey Mukhin on 26.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
+            }
+            .navigationBarTitle("Hacker News")
+        }
+        .onAppear(perform: {
+            self.networkManager.fetchData()
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Andrey Mukhin on 26.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
